2fd9b6c992249714943e4c7de8c4fd4c24527c0d
Yeshi <tyeshi777@yahoo.com>
2019-08-04 08:46:06 -0700

    added cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 // bring in the express-graphql module
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose"); // install mongoose to connect to mlab cloud database
 
 const server = express();
+
 // make the server use the graphqlHTTP module as a middleware to a single route. this is the one super charged endpoint to process all request.
 // its the function that fires whenever a request to this endpoint is made. this will handle graphql request. it takes an object with some options.
 mongoose.connect(
@@ -14,6 +16,8 @@ mongoose.connection.once("open", () => {
   // to verify that we are connected to cloud database
   console.log("connected to database");
 });
+
+server.use(cors());
 // after creating the mongoose connection we need to make the models for our database. So we are going to create a models folder.
 server.use(
   "/graphql",
